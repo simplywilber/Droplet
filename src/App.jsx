a39c256f5461f76ab4fdcafe55b022993e39a25d
simplywilber <simplywilber@gmail.com>
2026-02-28 21:05:59 -0800

    Install react router and create pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import SavedFortunes from "./pages/Fortunes";
 
 function App() {
-
   return (
     <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/fortunes" element={<SavedFortunes />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,4 @@
+function Home() {
+    return <h1>Home Page Test</h1>
+}
+ export default Home;
