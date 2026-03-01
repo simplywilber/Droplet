fc7e833b867078c381609197456f1c717ca88e78
simplywilber <simplywilber@gmail.com>
2026-03-01 01:45:25 -0800

    Add hover effects to links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav id="nav-bar">
-      <div>
-        <Link to="/" id="logo">Droplet</Link>
+      <div id="logo">
+        <Link class="link" to="/">Droplet</Link>
       </div>
       <div class="nav-links">
-        <Link to="/">Home</Link>
-        <Link to="/fortunes">Fortunes</Link>
-        <Link to="/about">About</Link>
+        <Link class="link" to="/">Home</Link>
+        <Link class="link" to="/fortunes">Fortunes</Link>
+        <Link class="link" to="/about">About</Link>
       </div>
     </nav>
   );
