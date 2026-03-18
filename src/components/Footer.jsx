a4c73344d02aa7bda01686048e786ca53d30d563
simplywilber <simplywilber@gmail.com>
2026-03-18 12:31:08 -0700

    Add forecast link to footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,14 +4,26 @@ function Footer() {
   const currentYear = new Date().getFullYear();
   return (
     <footer id="footer" aria-label="Site footer">
-      <Link className="link" to="/">Droplet</Link>
+      <Link className="link" to="/">
+        Droplet
+      </Link>
       <div>
         <p aria-hidden="true">|</p>
       </div>
       <nav id="footer-links" aria-label="Footer Navigation">
-        <Link className="link" to="/">Home</Link>
-        <Link className="link" to="/quotes">Quotes</Link>
-        <Link className="link" to="/about">About</Link>
+        <Link className="link" to="/">
+          Home
+        </Link>
+        <Link className="link" to="/quotes">
+          Quotes
+        </Link>
+        <Link className="link" to="/forecast">
+          Forecast
+        </Link>
+
+        <Link className="link" to="/about">
+          About
+        </Link>
         <p aria-hidden="true">|</p>
       </nav>
       <div>Wilber Amaya-Maurisio and Christian Velez &#169; {currentYear}</div>
