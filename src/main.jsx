81dd614e016d5449e9eaa8f0841131ab5aec5329
ChristianVelezW <Christian.VelezWit1@seattlecolleges.edu>
2026-03-18 11:02:23 -0700

    Implement WeatherContext for persistent location across pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import { WeatherProvider } from "./context/WeatherContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <WeatherProvider>
+          <App />
+        </WeatherProvider>
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
